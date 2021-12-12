629ed2efda6b2503c29b374db9099ab0b481b67c
Mohamed Samir <mohamad.samir08@gmail.com>
2021-12-12 13:16:46 +0200

    edit app.js file

--- app.js
+++ app.js
@@ -18,7 +18,5 @@ mongoose.connect('mongodb://localhost:27017/studentManger', {
 //use router middleware
 app.use(router);
 
-app.set('port', process.env.PORT || 3000);
-const server = app.listen(app.get('port'), () => {
-  console.log(`Server running at http://localhost:${app.get('port')}`);
-});
+if (process.env.NODE_ENV === 'test') app.set('port', 3001);
+else app.set('port', process.env.PORT || 3000);
